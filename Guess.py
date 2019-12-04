4fe349adeb8da6142b5217d064a74127cd0d801e
Last-Macen <34682983+Last-Macen@users.noreply.github.com>
2019-12-04 13:57:16 +0000

    Update Guess.py

--- Guess.py
+++ Guess.py
@@ -4,12 +4,12 @@ guess_count = 0
 guess_limit = 3
 limit = False
 
-while guess != secret_word and not limit:
-    if guess_count < guess_limit:
-        guess = input("Enter guess: ")
-        guess_count += 1
+while guess != secret_word and not limit: #while user guess is not right and limit is not true
+    if guess_count < guess_limit: 
+       guess = input("Enter guess: ")
+       guess_count += 1
     else :
-         limit = True
+         limit = True #change limit to True once count > guess limit
 if limit:
     print("You are out of guesses you lose")
 else:
